4cfed5f916df80977b0e110f07c6adc22e4cb0d3
Allions24 <142332557+Allions24@users.noreply.github.com>
2023-11-16 11:07:33 -0600

    matriz magica.cpp

--- matriz magica.cpp
+++ matriz magica.cpp
@@ -2,74 +2,60 @@
 void imprimirMatriz(int matriz[3][3]) 
 {
     for (int i = 0; i < 3; ++i) 
-	{
+    {
         for (int j = 0; j < 3; ++j) 
-		{
+        {
             printf("%d\t", matriz[i][j]);
-		}
+        }
         printf("\n");
     }
 }
-int esMatrizMagica(int matriz[3][3]) 
+int sumaFila(int matriz[3][3], int fila) 
 {
-int sumaFila = matriz[0][0] + matriz[0][1] + matriz[0][2];
-for (int i = 1; i < 3; ++i) 
-	{
-    	int sumaFilaActual = matriz[i][0] + matriz[i][1] + matriz[i][2];
-    	if (sumaFilaActual != sumaFila) 
-		{
-            return 0; 
-        }
+    int suma = 0;
+    for (int j = 0; j < 3; ++j) 
+    {
+        suma += matriz[fila][j];
     }
-for (int j = 0; j < 3; ++j) 
-	{
-		int sumaColumna = matriz[0][j] + matriz[1][j] + matriz[2][j];
-		if (sumaColumna != sumaFila) 
-		{
-        	return 0; 
-        }
-    }
-int sumaDiagonalPrincipal = matriz[0][0] + matriz[1][1] + matriz[2][2];
-if (sumaDiagonalPrincipal != sumaFila) 
-	{
-    return 0; 
-    }
-int sumaDiagonalSecundaria = matriz[0][2] + matriz[1][1] + matriz[2][0];
-if (sumaDiagonalSecundaria != sumaFila) 
-	{
-    return 0; 
-    }
-return 1; 
+    return suma;
 }
-
+int sumaColumna(int matriz[3][3], int columna) 
+{
+    int suma = 0;
+    for (int i = 0; i < 3; ++i) 
+    {
+        suma += matriz[i][columna];
+    }
+    return suma;
+}
 int main() 
 {
-int matriz[3][3];
-matriz[0][0] = 2;
-matriz[0][1] = 9;
-matriz[0][2] = 4;
-matriz[1][0] = 7;
-matriz[1][1] = 5;
-matriz[1][2] = 3;
-matriz[2][0] = 6;
-matriz[2][1] = 1;
-matriz[2][2] = 9;
-printf("Matriz ingresada:\n");
-imprimirMatriz(matriz);
-
-if (esMatrizMagica(matriz)) 
-	{
-		printf("La matriz es magica.\n");
-    } 
-else 
-	{
- 	 printf("La matriz no es magica.\n");
+    int matriz[3][3];
+    matriz[0][0] = 2;
+    matriz[0][1] = 9;
+    matriz[0][2] = 4;
+    matriz[1][0] = 7;
+    matriz[1][1] = 5;
+    matriz[1][2] = 3;
+    matriz[2][0] = 6;
+    matriz[2][1] = 1;
+    matriz[2][2] = 8;
+    printf("Matriz ingresada:\n");
+    imprimirMatriz(matriz);
+    
+    for (int i = 0; i < 3; ++i) 
+    {
+        printf("Suma de la fila %d: %d\n", i + 1, sumaFila(matriz, i));
     }
-    return 0;
+    for (int j = 0; j < 3; ++j) 
+    {
+        printf("Suma de la columna %d: %d\n", j + 1, sumaColumna(matriz, j));
+    }
+    int sumaDiagonalPrincipal = matriz[0][0] + matriz[1][1] + matriz[2][2];
+    printf("Suma de la diagonal principal: %d\n", sumaDiagonalPrincipal);
+    int sumaDiagonalSecundaria = matriz[0][2] + matriz[1][1] + matriz[2][0];
+    printf("Suma de la diagonal secundaria: %d\n", sumaDiagonalSecundaria);
+	
+return 0;
 }
-
-
-
-
-
  
